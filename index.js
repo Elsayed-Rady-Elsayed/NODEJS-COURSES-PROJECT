729d7bf0fc0f9d2907cfcbc9affe2a3f2d0cd97f
Elsayed-Rady-Elsayed <sayedrady61@gmail.com>
2024-11-20 00:43:46 +0200

    start project

--- index.js
+++ index.js
@@ -1,5 +1,22 @@
 const express = require("express");
+
 const app = express();
+
+const courses = [
+  { id: 1, title: "reactjs course", price: 650 },
+  { id: 2, title: "mongo course", price: 350 },
+  { id: 3, title: "nodejs course", price: 550 },
+];
+
+app.get("/api/courses", (req, res) => {
+  res.json(courses);
+});
+
+app.get("/api/courses/:id", (req, res) => {
+  const id = req.params.id;
+  const course = courses.find((course) => course.id === parseInt(id));
+  res.json(course);
+});
 
 app.listen(5000, () => {
   console.log("app running on port:5000");
