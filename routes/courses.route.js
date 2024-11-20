11a5803c32d738a0519dce6e768e15cae9fa0ad4
Elsayed-Rady-Elsayed <sayedrady61@gmail.com>
2024-11-20 02:54:32 +0200

    finishing

--- routes/courses.route.js
+++ routes/courses.route.js
@@ -6,25 +6,25 @@ const coursesController = require("../controllers/courses-controller");
 
 const router = express.Router();
 
-router.get("/", coursesController.getCourses);
+router
+  .route("/")
+  .get(coursesController.getCourses)
+  .post(
+    [
+      body("title")
+        .notEmpty()
+        .withMessage("enter valid title")
+        .isLength({ min: 5 })
+        .withMessage("enter title with more than 5 characters"),
+      body("price").notEmpty().withMessage("enter valid price"),
+    ],
+    coursesController.addCourse
+  );
 
-router.get("/:id", coursesController.getCourse);
-
-router.post(
-  "/",
-  [
-    body("title")
-      .notEmpty()
-      .withMessage("enter valid title")
-      .isLength({ min: 5 })
-      .withMessage("enter title with more than 5 characters"),
-    body("price").notEmpty().withMessage("enter valid price"),
-  ],
-  coursesController.addCourse
-);
-
-router.patch("/:id", coursesController.updateCourse);
-
-router.delete("/:id", coursesController.deletCourse);
+router
+  .route("/:id")
+  .get(coursesController.getCourse)
+  .patch(coursesController.updateCourse)
+  .delete(coursesController.deletCourse);
 
 module.exports = router;
