63991d8a0d0e88bd27301e64c9c7ae8dbfb3f4ee
Elsayed-Rady-Elsayed <sayedrady61@gmail.com>
2024-11-20 12:57:14 +0200

    normal mongo

--- index.js
+++ index.js
@@ -1,5 +1,19 @@
 const express = require("express");
 
+const mongoose = require("mongoose");
+
+const url =
+  "mongodb+srv://sayedrady61:012301050180@courses.b9h82.mongodb.net/proj-2";
+
+mongoose
+  .connect(url)
+  .then(() => {
+    console.log("connected successfully");
+  })
+  .catch((e) => {
+    console.log(e);
+  });
+
 const app = express();
 
 app.use(express.json());
