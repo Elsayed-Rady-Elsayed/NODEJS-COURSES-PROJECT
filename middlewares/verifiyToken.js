fbadbc9aa8747fbc5659d26f9b91de7fe22ecd80
Elsayed-Rady-Elsayed <sayedrady61@gmail.com>
2024-11-21 20:16:47 +0200

    add auth

--- middlewares/verifiyToken.js
+++ middlewares/verifiyToken.js
@@ -5,12 +5,16 @@ const appError = require("../utils/appError");
 const verifyToken = (req, res, next) => {
   const authToken =
     req.headers["Authorization"] || req.headers["authorization"];
+  if (!authToken) {
+    const error = appError.create("token is required", 401, FAIL);
+    return next(error);
+  }
   const token = authToken.split(" ")[1];
   try {
     const decodedToken = jwt.verify(token, process.env.JWT_SECRET);
     next();
   } catch (err) {
-    const error = appError.create("error with token", 404, FAIL);
+    const error = appError.create("error with token", 401, FAIL);
     return next(error);
   }
 };
